a7a57526a5a829bd9dfae5eeabd6de7800a56bf2
Andrew Pfalz <apfalztemp@fullsail.com>
2018-10-07 20:55:35 -0400

    fix fn bug

--- shared_functions.py
+++ shared_functions.py
@@ -1127,7 +1127,7 @@ class Training_Manager:
         self.seed_cursor    += 1
         self.seed_cursor    %= len(self.seed_list)
         if self.save_seeds == True:
-            fn = 'seed_' + str(self.train_cursor) + .'wav'
+            fn = 'seed_' + str(self.train_cursor) + '.wav'
             wav.write(fn, self.sampling_rate, np.hstack(np.array(output)))
             print('saved ' + fn)
 
